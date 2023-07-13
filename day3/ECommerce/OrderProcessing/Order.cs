5dbf2f908e51d825f7790bc1774060bb86de0d81
Ashish Mahajan <aashishmahajan01@gmail.com>
2023-07-13 21:55:04 +0530

    day3

---
+++ day3/ECommerce/OrderProcessing/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderProcessing;
+public class Order
+{
+    public int Id{get;set;}
+    public DateTime OrderDate{get;set;}
+    public double SubTotal{get;set;}
+    public string Status{get;set;}
+}
---
+++ day3/ECommerce/TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Catalog;
+using CRM;
+using Membership;
+using OrderProcessing;
+using ShoppingCart;
+
+
+//Property Initializer
+Product p1=new Product{ Id=12,
+                        Title="Gerbera",
+                         Description="Wedding Flower",
+                         UnitPrice=10,
+                         Quantity=56000};
+
+p1.Title="Jasmine";
+Console.WriteLine(p1.Id + " "+ p1.Title + " "+ p1.Description);
